6eb4559886126bcaddbfa978d648cf44884a6e96
milade3013 <milade3013@gmail.com>
2022-11-08 09:58:11 +0330

    touch requirements during install

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="filecoin-lotus",
     version="1.0.0",
@@ -20,5 +23,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    python_requires=">=3.6"
+    python_requires=">=3.6",
+    install_requires=required,
 )
